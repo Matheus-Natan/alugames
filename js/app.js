640335339eb0e4226bd03206fdd99c0491bdf197
MATHEUS NATAN <m.gomesmg1904@gmail.com>
2025-03-17 11:06:09 -0300

    Adicionando confirmação na hora de devolver o jogo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 function alterarStatus(id) {
     let jogo = document.getElementById(`game-${id}`);
     let imagem = jogo.querySelector('.dashboard__item__img');
-    let botao = jogo.querySelector(".dashboard__item__button");
+    let botao = jogo.querySelector('.dashboard__item__button');
+    let nomeJogo = jogo.querySelector('.dashboard__item__name')
     
     if (imagem.classList.contains('dashboard__item__img--rented') && botao.classList.contains('dashboard__item__button--return')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.innerHTML = "Alugar";
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.innerHTML = "Alugar";
+        } 
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
